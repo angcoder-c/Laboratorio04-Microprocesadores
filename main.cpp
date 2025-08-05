6d5cdaa01540090f64d2028a303b38fb0bb6240b
angcoder-c <angcoder.c@gmail.com>
2025-08-05 07:11:31 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Angel Gabriel Chavez Otzoy
+ * 24248
+ * Programacion de microprocesadores
+ * Laboratorio 04
+ * Fecha: 05 / 08 / 2025
+ */
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
